4531a428519a1de656b6adadfc0947beb0e954f6
Anna-Drokina <91247674+Anna-Drokina@users.noreply.github.com>
2021-09-25 00:21:45 +0300

    Add new file

---
+++ game.py
@@ -0,0 +1,61 @@
+print('Tic-tac-toe game')
+board = [' ' for j in range(9)]
+
+
+def draw_board(board):
+    for i in range(3):
+        print(board[0 + i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3])
+        if i < 2:
+            print('-' * 9)
+
+
+def take_input(player_token):
+    valid = False
+    while not valid:
+        player_answer = input("Where to put the " + player_token+"? ")
+        try:
+            player_answer = int(player_answer)
+        except ValueError:
+            print("Incorrect input. Are you sure you entered a number?")
+            continue
+        if 1 <= player_answer <= 9:
+            if str(board[player_answer-1]) not in "XO":
+                board[player_answer-1] = player_token
+                valid = True
+            else:
+                print("This cell is already occupied!")
+        else:
+            print("Incorrect input. Enter a number from 1 to 9.")
+
+
+def check_win(board):
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),
+                 (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1]] == board[each[2]] == 'X' or \
+                board[each[0]] == board[each[1]] == board[each[2]] == 'O':
+            return board[each[0]]
+    return False
+
+
+def main(board):
+    counter = 0
+    win = False
+    while not win:
+        draw_board(board)
+        take_input("X") if counter % 2 == 0 else take_input("O")
+        counter += 1
+        if counter > 4:
+            tmp = check_win(board)
+            if tmp:
+                print(tmp, "win!")
+                break
+        if counter == 9:
+            print("Draw!")
+            break
+    draw_board(board)
+
+
+main(board)
+
+input("Press Enter to exit!")
